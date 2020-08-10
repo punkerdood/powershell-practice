9e02601945eaadee8b0cefef6f8402ec4e7aac33
Punkerdood <majpatmcd@gmail.com>
2020-08-10 14:29:19 -0400

    auto committed from auto-committing-setup.ps1!

--- auto-committing-setup.ps1
+++ auto-committing-setup.ps1
@@ -1,6 +1,6 @@
 # declare variables
 $GitHubUsername='punkerdood'
-$PracticeRepoDir="$(Get-Location)\Studio1"
+$PracticeRepoDir="$(Get-Location)\Studio"
 $CommitMessage='auto committed from auto-committing-setup.ps1!'
 
 
@@ -10,11 +10,11 @@ New-Item -Name "Studio" -ItemType "directory" -Path $(Get-Location)
 
 # fork and clone this repo into the powershell-studio directory
 git clone "https://github.com/$GitHubUsername/powershell-practice" "$PracticeRepoDir"
-Set-Location -Path "Studio1"
+Set-Location -Path "Studio"
 Copy-Item -Path "../auto-committing-setup.ps1"
 git add auto-committing-setup.ps1
 
-git commit -m Write-Output $CommitMessage
+git commit -m "$CommitMessage"
 git push 
 
 
